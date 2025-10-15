6b6a95a1a18b6cbf8a5955bcb7ced8fa5666513a
Rodrigomuina <Rodrigoemanuelmuina991@gmail.com>
2025-10-15 04:11:34 -0300

    camara_arreglada

--- Assets/Scripts/Core/CameraManager.cs
+++ Assets/Scripts/Core/CameraManager.cs
@@ -1,8 +1,9 @@
+using StarterAssets;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    
+    public ThirdPersonController playerController;
     public GameObject mainCamera;
     public GameObject playerMainCamera;
     public GameObject firstPersonCamera;
@@ -28,7 +29,7 @@ public class CameraManager : MonoBehaviour
         firstPersonCamera.SetActive(firstPerson);
         mainCamera.SetActive(!firstPerson);
         playerMainCamera.SetActive(!firstPerson);
-
+        playerController.isFirstPerson = firstPerson;
         Debug.Log($"Cámara activa: {(firstPerson ? "1ra persona" : "3ra persona")}");
     }
 }
